27465046b32231fa029e9c4c270017c2beed0fd8
Joshua Retallick <josh@old-empire.co.uk>
2021-02-18 12:35:40 +0000

    Cleared project ready for next exercise

--- lib/kata_practice.rb
+++ lib/kata_practice.rb
@@ -1,8 +1,8 @@
-def bonus_time(salary, bonus)
-  if bonus == false
-    return "$#{salary}"
-  else
-    bonus_pay = salary * 10
-    return "$#{bonus_pay}"
-  end
-end
+# def bonus_time(salary, bonus)
+#   if bonus == false
+#     return "$#{salary}"
+#   else
+#     bonus_pay = salary * 10
+#     return "$#{bonus_pay}"
+#   end
+# end
--- spec/kata_practice_spec.rb
+++ spec/kata_practice_spec.rb
@@ -1,19 +1,19 @@
-require 'kata_practice'
-
-describe "#bonus_time" do
-  it "return the inputted salary (10000) if no bonus is false" do
-    expect(bonus_time(10000, false)).to eq "$10000"
-  end
-
-  it "return the inputted salary (10000) * 10 if bonus is true" do
-    expect(bonus_time(10000, true)).to eq "$100000"
-  end
-
-  it "return the inputted salary (60000) if no bonus is false" do
-    expect(bonus_time(60000, false)).to eq "$60000"
-  end
-
-  it "return the inputted salary (25000) * 10 if bonus is true" do
-    expect(bonus_time(25000, true)).to eq "$250000"
-  end
-end
+# require 'kata_practice'
+#
+# describe "#bonus_time" do
+#   it "return the inputted salary (10000) if no bonus is false" do
+#     expect(bonus_time(10000, false)).to eq "$10000"
+#   end
+#
+#   it "return the inputted salary (10000) * 10 if bonus is true" do
+#     expect(bonus_time(10000, true)).to eq "$100000"
+#   end
+#
+#   it "return the inputted salary (60000) if no bonus is false" do
+#     expect(bonus_time(60000, false)).to eq "$60000"
+#   end
+#
+#   it "return the inputted salary (25000) * 10 if bonus is true" do
+#     expect(bonus_time(25000, true)).to eq "$250000"
+#   end
+# end
