cc577a86df42f657cf70e711e7827f95041824c5
Joshua Retallick <josh@old-empire.co.uk>
2021-02-23 13:17:06 +0000

    Reset

--- spec/kata_practice_spec.rb
+++ spec/kata_practice_spec.rb
@@ -1,13 +1,3 @@
 require 'kata_practice'
 
-describe "#array_diff" do
-
-  it "returns the original array, if second array is empty" do
-    expect(array_diff([1],[])).to eq [1]
-  end
-
-  it "returns new array if a & b do not share any values" do
-    expect(array_diff([1,2],[3])).to eq [1,2,3]
-  end
-
 end
