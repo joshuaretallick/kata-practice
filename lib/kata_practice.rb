401a1180e44efd420138c8b4eba004d7dc8406fa
Joshua Retallick <josh@old-empire.co.uk>
2021-02-24 09:46:53 +0000

    test one

--- lib/kata_practice.rb
+++ lib/kata_practice.rb
@@ -1,14 +1,3 @@
-class Shopping
-
-  A = 50
-  B = 30
-  C = 20
-  D = 15
-
-  def checkout(a)
-    sum = 0
-    sum += a
-    return sum
-  end
-
+def leapyear(year)
+  true
 end
--- spec/kata_practice_spec.rb
+++ spec/kata_practice_spec.rb
@@ -1,3 +1,7 @@
 require 'kata_practice'
 
+describe "#leapyear" do
+  it "returns true when input is 800 because it is a leapyear" do
+    expect(leapyear(800)).to eq true
+  end
 end
