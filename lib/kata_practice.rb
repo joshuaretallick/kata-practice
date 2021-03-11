b2f4a2e2800f8c8f45b439f65955c8ac8668935b
Joshua Retallick <josh@old-empire.co.uk>
2021-03-11 12:21:30 +0000

    checkPalindrome

--- lib/kata_practice.rb
+++ lib/kata_practice.rb
@@ -1,7 +1,7 @@
-def centuryFromYear(year)
-    if year % 100 == 0
-        return year / 100
-    else
-        return (year / 100) + 1
-    end
+def checkPalindrome(inputString)
+  if inputString.reverse == inputString
+    return true
+  else
+    return false
+  end
 end
--- spec/kata_practice_spec.rb
+++ spec/kata_practice_spec.rb
@@ -1,13 +1,17 @@
 require 'kata_practice'
 
-describe "#centuryFromYear" do
-  it "returns the century when the input Year is entered" do
-    expect(centuryFromYear(1905)).to eq 20
+describe "#checkPalindrome" do
+
+  it "returns true if the string reversed is the same as it is forwards" do
+    expect(checkPalindrome("aabaa")).to eq true
   end
 
-  it "returns the century when the input Year is entered" do
-    expect(centuryFromYear(1700)).to eq 17
+  it "returns false if the string reversed is not the same as it is forwards" do
+    expect(checkPalindrome("abac")).to eq false
   end
 
+  it "returns true if the string reversed is the same as it is forwards even for single letters" do
+    expect(checkPalindrome("a")).to eq true
+  end
 
 end
