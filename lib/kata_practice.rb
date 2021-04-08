99acdfec38a47cf534b52effdee88c51f58990dc
Joshua Retallick <josh@old-empire.co.uk>
2021-04-08 14:27:31 +0100

    T10

--- lib/kata_practice.rb
+++ lib/kata_practice.rb
@@ -1,3 +1,7 @@
 def square_sum(numbers)
-  9
+  numbers.map do |i| i ** 2
+  end.reduce(:+)
 end
+
+# Get each element of the array and square it, then return to check - DONE
+# Add all the elements of array together
